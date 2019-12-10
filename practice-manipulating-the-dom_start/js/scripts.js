47d082ac5416b59bb180206c4e7417f94764fdb7
Jesse Gay <jesse@teamtreehouse.com>
2019-12-09 16:26:38 -0800

    First 3 questions

--- practice-manipulating-the-dom_start/js/scripts.js
+++ practice-manipulating-the-dom_start/js/scripts.js
@@ -1,12 +1,15 @@
 // 1: Set the text of the <h1> element
-
+let text = document.getElementsByTagName('h1')[0];
+text.innerHTML = 'Fun stuff to do!';
 
 // 2: Set the color of the <h1> to a different color
-
+const headline = document.querySelector('h1');
+headline.style.color = 'lime';
 
 // 3: Set the content of the '.desc' paragraph
 // The content should include at least one HTML tag
-
+const description = document.querySelector('.desc');
+description.innerHTML = 'This is a list of <strong>fun</strong> stuff I want to do today';
 
 // 4: Set the class of the <ul> to 'list'
 
