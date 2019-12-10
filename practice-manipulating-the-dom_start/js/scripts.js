9a04623dc3baeb2405e71e1ac4b7a9011b359e6f
Jesse Gay <jesse@teamtreehouse.com>
2019-12-09 17:19:07 -0800

    4 through 7

--- practice-manipulating-the-dom_start/js/scripts.js
+++ practice-manipulating-the-dom_start/js/scripts.js
@@ -1,27 +1,38 @@
 // 1: Set the text of the <h1> element
-let text = document.getElementsByTagName('h1')[0];
-text.innerHTML = 'Fun stuff to do!';
+let title = document.getElementsByTagName('h1')[0];
+title.innerHTML = 'Fun stuff to do!';
 
 // 2: Set the color of the <h1> to a different color
-const headline = document.querySelector('h1');
-headline.style.color = 'lime';
+title.style.color = 'lime';
 
 // 3: Set the content of the '.desc' paragraph
 // The content should include at least one HTML tag
 const description = document.querySelector('.desc');
-description.innerHTML = 'This is a list of <strong>fun</strong> stuff I want to do today';
+description.innerHTML = 'This is a list of <strong><em>fun</strong></em> stuff I want to do today';
 
 // 4: Set the class of the <ul> to 'list'
-
+const list = document.querySelector('ul');
+list.className = 'list';
 
 // 5: Create a new list item and add it to the <ul>
-
+//const surf = 'Surf';
+const item = document.createElement('li');
+item.innerHTML = '<input> Surf';
+list.appendChild(item);
 
 // 6: Change all <input> elements from text fields to checkboxes
-
-
+// for (var i = 0; i < list.length; i += 1) {
+//     list[i].type = 'checkbox';
+// }
+const inputs = document.querySelectorAll('input');
+for (var i = 0; i < inputs.length; i += 1) {
+    inputs[i].type = 'checkbox';
+}
 // 7: Create a <button> element, and set its text to 'Delete'
 // Add the <button> inside the '.extra' <div>
-
+const deleteButton = document.createElement('button');
+deleteButton.innerHTML = 'Delete';
+const extra = document.querySelector('div .extra');
+extra.appendChild(deleteButton);
 
 // 8: Remove the '.extra' <div> element from the DOM when a user clicks the 'Delete' button
